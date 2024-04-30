d6a1bb013468f0bc0a0af002a89f244aec946872
fahim.ahmed <fahim.ahmed@japantravel.com>
2024-04-30 15:48:15 +0600

    Customize user list in admin panel

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import CustomUser
 
 
-admin.site.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email", "first_name", "last_name", "birth_date"]
+
+    def birth_date(self, obj):
+        return obj.date_of_birth.strftime('%b %d, %Y')
+
+admin.site.register(CustomUser, UserAdmin)
